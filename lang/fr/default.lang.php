91e09f493a156f860651cbc3427b142ade299de5
foine <brousse@novius.fr>
2014-03-25 14:14:16 +0100

    Add translation for the administration panel

---
+++ lang/fr/default.lang.php
@@ -0,0 +1,59 @@
+<?php
+
+// Generated on 25/03/2014 13:48:03
+
+// 16 out of 16 messages are translated (100%).
+// 88 out of 88 words are translated (100%).
+
+return array(
+    #: config/controller/admin/config.config.php:3
+    'Simple Twitter cards' => 'Simple Twitter cards',
+
+    #: config/controller/admin/config.config.php:15
+    'Général' => 'Général',
+
+    #: config/controller/admin/config.config.php:39
+    'Images' => 'Images',
+
+    #: config/controller/admin/config.config.php:61
+    'Choisissez votre type de card :' => 'Choisissez votre type de card :',
+
+    #: config/controller/admin/config.config.php:65
+    'Résumé' => 'Résumé',
+
+    #: config/controller/admin/config.config.php:66
+    'Résumé à grande image' => 'Résumé à grande image',
+
+    #: config/controller/admin/config.config.php:67
+    'Photo' => 'Photo',
+
+    #: config/controller/admin/config.config.php:72
+    'Entrez votre compte Twitter personnel :' => 'Compte Twitter de l\'auteur du contenu :',
+
+    #: config/controller/admin/config.config.php:78
+    'Entrez le compte Twitter associé au site web :' => 'Compte Twitter du détenteur du domaine :',
+
+    #: config/controller/admin/config.config.php:84
+    'Choisissez la taille de l\'image :' => 'Choisissez la taille de l\'image :',
+
+    #: config/controller/admin/config.config.php:88
+    'Mobile à affichage non retina (largeur: 280px - longueur: 375px)' => 'Mobile à affichage non retina (largeur: 280px - longueur: 375px)',
+
+    #: config/controller/admin/config.config.php:89
+    'Mobile à affichage retina (largeur: 560px - longueur: 750px)' => 'Mobile à affichage retina (largeur: 560px - longueur: 750px)',
+
+    #: config/controller/admin/config.config.php:90
+    'Taille max pour le web (largeur: 435px - longueur: 375px)' => 'Taille max pour le web (largeur: 435px - longueur: 375px)',
+
+    #: config/controller/admin/config.config.php:91
+    'Petit (largeur: 280px - longueur: 150px)' => 'Petit (largeur: 280px - longueur: 150px)',
+
+    #: config/controller/admin/config.config.php:96
+    'Forcer le recadrage de l\'image de la card :' => 'Forcer le recadrage de l\'image de la card :',
+
+    #: config/controller/admin/config.config.php:104
+    'Image par défaut des cards :' => 'Image par défaut des cards :',
+
+    '(optionnel)' => '(optionnel)',
+
+);
