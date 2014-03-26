8dba841e8c1c3d8023e62cff7155192358417613
foine <brousse@novius.fr>
2014-03-26 17:36:24 +0100

    Add view files (forget to commit)

---
+++ views/admin/validator.view.php
@@ -0,0 +1,7 @@
+<?= __('To validate your card, you need to go to:') ?>
+<br/>
+<a href="https://dev.twitter.com/docs/cards/validation/validator"><?= __('Twitter cards validator') ?></a>
+<br/>
+<?= __('Here just sign in and copy the url of one of your page or item that use this application.') ?>
+<br/>
+<?= __('Twitter will check that you own the domain and validate it almost immediatly. After that, you will see twitter card into your tweets.') ?>
